7fee8235064564431319aec9d23b216a650781f0
brohrer <brohrer@gmail.com>
2022-07-10 15:25:49 -0400

    logging working

--- model/v00.py
+++ model/v00.py
@@ -1,10 +1,33 @@
+import json
+import logging
+import os
+import time
 from pacemaker.v00 import Pacemaker
 
 CLOCK_FREQ_HZ = 3
+# valid levels are {DEBUG, INFO, WARNING, ERROR, CRITICAL}
+LOGGING_LEVEL = logging.DEBUG
+
 
 class Model:
-    def __init__(self):
+    def __init__(self, name="_"):
         self.pacemaker = Pacemaker(CLOCK_FREQ_HZ)
 
+        # Set up logging
+        os.makedirs("log", exist_ok=True)
+        logging.basicConfig(
+            filename=os.path.join("log", f"{name}_model.log"),
+            format='%(message)s')
+
+        self.logger = logging.getLogger()
+        # levels = {DEBUG, INFO, WARNING, ERROR, CRITICAL}
+        self.logger.setLevel(LOGGING_LEVEL)
+
     def run(self):
-        self.pacemaker.beat()
+        over = self.pacemaker.beat()
+        self.logger.debug(json.dumps({
+            "level": "DEBUG",
+            "over": over,
+            "time": time.time(),
+        }))
+
--- play.py
+++ play.py
@@ -1,9 +1,11 @@
+import time
 from key_reader.v00 import Reader
 from model.v00 import Model
 from world.v00 import World
 from animation.v00 import Animation
 
-model = Model()
+name = f"{int(time.time())}"
+model = Model(name)
 
 for _ in range(1000):
     model.run()
